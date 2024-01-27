04c64a3d6f792295a0b3031b5ab772e3874a076c
Trickster <reone054@gmail.com>
2024-01-27 20:49:06 +0545

    random-color generation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Accordian from './components/accordian';
-
+import RandomColor from './components/random color/index.jsx';
 function App() {
   return (
     <div className="App">
       {/* Accordian*/}
       <Accordian/>
+      {/* Generating Random Color*/}
+      <RandomColor/>
     </div>
   )
 }
