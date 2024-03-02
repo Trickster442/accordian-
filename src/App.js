fbd55f6c0786f2da31149bb9791ae0e039660bb3
Trickster <reone054@gmail.com>
2024-03-02 20:18:47 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import RandomColor from './components/random color/index.jsx';
 import StarRating from './components/star rating/index.jsx';
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       {/* Accordian */}
-      {/* <Accordian/> */}
+      <Accordian/>
       {/* Generating Random Color*/}
-      {/* <RandomColor/> */}
+      <RandomColor/>
 
       {/*Creating Star-Rating Page*/ }
-      {/* <StarRating /> */}
+      <StarRating />
 
       {/*Image-slider project where url is passing through props*/}
       <ImageSlider url={'https://picsum.photos/v2/list?page=1'} limit={'10'} page={'1'}/>
